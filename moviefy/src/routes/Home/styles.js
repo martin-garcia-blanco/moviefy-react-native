cb3d7e19b982bee96b168f468c810248d1744b9d
Martin Garcia Blanco <martin-garcia-blanco@MacBook-Pro.local>
2020-07-13 23:11:00 +0200

    feat(fonts): Add new custom font

--- moviefy/src/routes/Home/styles.js
+++ moviefy/src/routes/Home/styles.js
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
 import { blackColor } from '../../helpers/GlobalStyles';
+import { heightScreen } from '../..//helpers/GlobalStyles';
 
 export default StyleSheet.create({
   scrollView: {
     backgroundColor: blackColor,
     paddingTop: 10,
   },
+  list: {},
 });
