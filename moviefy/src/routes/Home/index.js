a21a9a36917dd8ed902d3635363701967abc1d48
Martín García Blanco <programadopormartin@gmail.com>
2021-01-02 15:12:48 +0100

    ✨ Create new hook for initial movies

--- moviefy/src/routes/Home/index.js
+++ moviefy/src/routes/Home/index.js
@@ -1,29 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ScrollView, View } from 'react-native';
-import MovieList from '../../views/MovieList';
-import styles from './styles';
-import retrieveInitialMovies from '../../services/retrieve-initial-movies';
-import Header from '../../views/Header/Header';
-import TMDB from '../../views/TMDB/TMDB';
-import PreHome from '../../views/PreHome/PreHome';
+import useRetrieveInitialMovies from '../../hooks/useRetrieveInitialMovies';
 import Error from '../../views/Error/Error';
+import Header from '../../views/Header/Header';
+import MovieList from '../../views/MovieList';
+import PreHome from '../../views/PreHome/PreHome';
+import TMDB from '../../views/TMDB/TMDB';
+import styles from './styles';
 
 const Home = ({ navigation }) => {
-  const [popularMovies, setPopularMovies] = useState();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        setPopularMovies(await retrieveInitialMovies());
-        setLoading(false);
-      } catch (e) {
-        setLoading(false);
-        setError(true);
-      }
-    })();
-  }, [setPopularMovies]);
+  const [popularMovies, loading, error] = useRetrieveInitialMovies();
 
   return (
     <ScrollView style={styles.scrollView}>
