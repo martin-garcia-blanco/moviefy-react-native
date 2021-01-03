40560841e8252032f53a009d2531f296a28f1d15
Martín García Blanco <programadopormartin@gmail.com>
2021-01-03 19:06:24 +0100

    ✨ Add logic to show movies filtered by job

--- moviefy/src/views/Person/styles.js
+++ moviefy/src/views/Person/styles.js
@@ -22,7 +22,39 @@ export default StyleSheet.create({
   biography: {
     marginTop: 10,
   },
-  movies: {},
+  movies: {
+    marginTop: 20,
+  },
+  departmentWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  departmentOption: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginHorizontal: 5,
+    marginVertical: 5,
+    borderColor: 'white',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  movieList: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  option20: {
+    width: '20%',
+  },
+  option40: {
+    width: '50%',
+  },
   text: {
     color: '#FFFFFF',
   },
